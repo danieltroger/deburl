adecd5f5139dfec52b1f8bbe2b540d6559776c17
Daniel Troger <daniel@natur-kultur.eu>
2014-07-01 18:59:16 +0200

    finding the line where the section of a package starts works now

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
-echo getpkglist("http://repo.natur-kultur.eu"); // http://apt.saurik.com/dists/ios/main/binary-darwin-arm/
+var_dump(find(getpkglist("http://repo.natur-kultur.eu"),"Package","eu.danieltroger.stether")); // http://apt.saurik.com/dists/ios/main/binary-darwin-arm/
 function getpkglist($repourl)
 {
 $repourl = validate($repourl);
@@ -13,11 +13,11 @@ die("Couldn't find packages listing");
 $rawlist = curl($pkgurl);
 if($pkglt == "Packages")
 {
-return $rawlist;
+return fix($rawlist);
 }
 elseif($pkglt == "Packages.bz2")
 {
-return bzdecompress($rawlist);
+return fix(bzdecompress($rawlist));
 }
 elseif($pkglt == "Packages.gz")
 {
@@ -31,7 +31,7 @@ $out .= gzread($file, $buffer_size);
 }
 gzclose($file);
 unlink($tmp);
-return $out;
+return fix($out);
 }
 }
 function validate($repourl)
@@ -71,10 +71,6 @@ else
 return true;
 }
 }
-function p($a)
-{
-return explode(":",$a);
-}
 function curl($url)
 {
 $ch = curl_init( $url );
@@ -86,4 +82,31 @@ $response = preg_split( '/([\r\n][\r\n])\\1/', curl_exec( $ch ));
 $response = preg_split( '/([\r\n][\r\n]){2}/', curl_exec( $ch ),2);
 curl_close( $ch );
 return $response[1];
+}
+function find($haystack,$key,$value,$start = 0)
+{
+$lines = explode("\n",$haystack);
+for($index = $start;$index < (sizeof($lines) -1);$index++)
+{
+$line = $lines[$index];
+$splitted = explode(":",$line);
+$splitted[1] = substr($splitted[1],1);
+if($splitted[0] == $key)
+{
+if($splitted[1] == $value)
+{
+echo "{$splitted[1]} matched {$value}\n";
+return $index;
+}
+else
+{
+echo "{$splitted[1]} didn't match {$value}\n";
+}
+}
+}
+return false;
+}
+function fix($a)
+{
+return str_replace("\r","",$a);
 }
